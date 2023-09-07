40c734bde9f76a70e0037436604bc91d7823d5b9
Moinul Islam <moinulilm10@gmail.com>
2023-09-07 19:38:25 +0600

    react-router-dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Signup from "./components/Pages/Signup";
+import { Home } from "./components/Pages/Home";
+import { Login } from "./components/Pages/Login";
+import { UserProfile } from "./components/Pages/UserProfile";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Signup />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+      </Routes>
     </>
   );
 }
